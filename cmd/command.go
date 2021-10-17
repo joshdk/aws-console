cd61ad80e091b01d14df6381a9747f55ffbaa979
Josh Komoroske <jdkomo@gmail.com>
2021-10-17 00:01:15 -0700

    feat: retrieve credentials from aws cli profile (#4)

--- cmd/command.go
+++ cmd/command.go
@@ -3,13 +3,24 @@
 // a copy of which can be found in the LICENSE.txt file.
 // SPDX-License-Identifier: MIT
 
+// Package cmd contains functionality for supporting the aws-console cli.
 package cmd
 
 import (
+	"github.com/joshdk/aws-console/credentials"
 	"github.com/spf13/cobra"
 )
 
+type flags struct {
+	// profile is the name of profile used for retrieving credentials from the
+	// AWS cli config files.
+	profile string
+}
+
+// Command returns a complete handler for the aws-console cli.
 func Command() *cobra.Command {
+	var flags flags
+
 	cmd := &cobra.Command{
 		Use:  "aws-console [flags…] [profile]",
 		Long: "aws-console - Generate temporary login URLs for the AWS Console",
@@ -17,8 +28,19 @@ func Command() *cobra.Command {
 		SilenceUsage:  true,
 		SilenceErrors: true,
 
+		Args: cobra.MaximumNArgs(1),
+		PreRun: func(_ *cobra.Command, args []string) {
+			if len(args) >= 1 {
+				// As a convenience, determine profile name from cli args here.
+				flags.profile = args[0]
+			}
+		},
+
 		RunE: func(*cobra.Command, []string) error {
-			return nil
+			// Retrieve credentials from the AWS cli config files.
+			_, err := credentials.FromConfig(flags.profile)
+
+			return err
 		},
 	}
 
